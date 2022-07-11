4345f7ffd2600ecc72ae8eec01480296ee1163ea
Jibo V Mathew <72732741+jibro01@users.noreply.github.com>
2022-07-11 21:51:50 +0530

    Create minion.py

---
+++ minion.py
@@ -0,0 +1,16 @@
+def minion_game(string):
+    vw='aeiou'.upper()
+    strl=len(string)
+    kevin=sum(strl-i for i in range(strl) if string[i] in vw)
+    stuart=strl*(strl + 1)/2 - kevin
+    
+    if kevin == stuart:
+        print('Draw')
+    elif kevin > stuart:
+        print('Kevin %d' % kevin)
+    else:
+        print('Stuart %d' % stuart)
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
